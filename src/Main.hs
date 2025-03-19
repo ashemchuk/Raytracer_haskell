7bcef7561bf9951fd6c7427d1c281b3f6f287cfd
Anna Shemchuk <annashemchuk@Annas-MacBook-Air.local>
2025-03-19 13:58:35 +0700

    main in progress

---
+++ src/Main.hs
@@ -0,0 +1,19 @@
+-- module Main where
+import Shapes
+import Config
+import Renderable
+import Vector
+import Data.Maybe (fromMaybe)
+import Scene
+getRay :: Double -> Double -> Ray -- (x, y) - pixel
+getRay x y = Ray {from = camera , direction = normalise (sub (Vec3 x y 0) camera)}
+
+allRays = [uncurry getRay| x <- [0 .. (width - 1)], y <- [0 .. (height - 1)]]
+
+intersect :: Renderable a => Ray -> a -> Double
+intersect ray obj = fromMaybe (-1) $ trace ray obj
+
+getPixelColor :: Ray -> Color
+getPixelColor ray = color (scal (fst visible) (sub (direction ray) (from ray)) ) (snd visible)
+    where visible = minimum $ filter (\(p, o) -> p > 0) $  map (\o -> (intersect ray o, o)) scene
+        
